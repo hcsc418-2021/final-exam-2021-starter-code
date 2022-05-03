b1ce1b950dddb0fb39b6e83427252476ab9423e7
Nubee2000 <97996752+Nubee2000@users.noreply.github.com>
2022-05-02 22:22:19 -0400

    Updating functions

--- backend/tests/game_helper.test.js
+++ backend/tests/game_helper.test.js
@@ -14,5 +14,36 @@ describe("game helper unit tests", () => {
 		const result = gameHelper.play({player: 0, computer: 0})
 		expect(result).toBe('draw')
 	})
+
 	// TODO: write test to cover the other 8 cases
+	test('when player plays ROCK and computer plays paper, expect a computer wins', () => {
+		const result = gameHelper.play({player: 0, computer: 1})
+		expect(result).toBe('computer wins')
+	})
+
+	test('when player plays ROCK and computer plays CLIPPERS, expect a player wins', () => {
+		const result = gameHelper.play({player: 0, computer: 2})
+		expect(result).toBe('player wins')
+	})
+
+	test('when player plays PAPER and computer plays ROCK, expect a player wins', () => {
+		const result = gameHelper.play({player: 1, computer: 0})
+		expect(result).toBe('player wins')
+	})
+
+	test('when player plays PAPER and computer plays CLIPPERS, expect a computer wins', () => {
+		const result = gameHelper.play({player: 1, computer: 2})
+		expect(result).toBe('computer wins')
+	})
+
+	test('when player plays CLIPPERS and computer plays ROCK, expect a computer wins', () => {
+		const result = gameHelper.play({player: 2, computer: 0})
+		expect(result).toBe('computer wins')
+
+	})
+
+	test('when player plays CLIPPERS and computer plays PAPER, expect a player wins', () => {
+		const result = gameHelper.play({player: 2, computer: 1})
+		expect(result).toBe('player wins')
+	})
 })
